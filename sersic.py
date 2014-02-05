a5c084bf7a30fca405707d2f5874577d5aee4f53
Alex Hagen <mr.alex.hagen@gmail.com>
2014-02-05 12:20:26 -0500

    added sersic profile

---
+++ sersic.py
@@ -0,0 +1,15 @@
+# a module that implements sersic profiles with arbitrary n
+
+
+class Sersic(object):
+    """
+    sersic profiles! what fun! http://en.wikipedia.org/wiki/Sersic%27s_law
+    """
+    
+    def __init__(self, n, R0=1.0, I0=1.0):
+        """
+        creates a sersic profile object
+        """
+        
+        
+        

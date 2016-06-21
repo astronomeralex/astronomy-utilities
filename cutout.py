50f4a2d7bfd991e314b094ad1e0a12a936acba11
Alex Hagen <mr.alex.hagen@gmail.com>
2016-06-21 17:04:16 -0400

    cutout.py working!

--- cutout.py
+++ cutout.py
@@ -8,47 +8,62 @@ Generate a cutout image from a .fits file
 from astropy.io import fits
 from astropy import wcs
 import numpy
+import astropy
 
 class DimensionError(ValueError):
     pass
 
-def cutout(filename, xc, yc, xw=500, yw=500, outfile=None, clobber=True, verbose=False):
+def make_cutouts(fitsfilename, RA, DEC, name, xw, yw, fits_memmap=False):
+    """
+    fitsfile - fits file name
+    RA - array of ras in degrees
+    DEC - array of decs in degrees
+    name - array of names for output file
+    fits-memmap - should the fits file be memmaped (turn on if large)
+    """
+    fitswcs = wcs.WCS(fitsfilename)
+    fitsfile = fits.open(fitsfilename,memmap = fits_memmap)
+    xypix = fitswcs.all_world2pix(RA, DEC, 0)
+    for i in  range(len(name)):
+        cutout(fitsfile, xypix[0][i], xypix[1][i], xw, yw, name[i])
+    
+
+def cutout(filename, xc, yc, xw, yw, outfile, clobber=True, verbose=False):
     """
     Inputs:
         file  - .fits filename or pyfits HDUList (must be 2D)
-        xc,yc - x and y coordinates in the fits files' coordinate system (CTYPE)
-        xw,yw - x and y width (pixels or wcs)
-        units - specify units to use: either pixels or wcs
-        outfile - optional output file
+        xc,yc - x and y coordinates in the fits files' coordinate system 
+        xw,yw - x and y width in pixels 
+        outfile - output file name
+
+        #how should i handle boundary cases? fill outside image with zeros?
     """
     if isinstance(filename,str):
-        file = fits.open(filename)
+        fitsfile = fits.open(filename)
         opened=True
-    elif isinstance(filename,fits.HDUList): #check type
-        file = filename
+    elif isinstance(filename,astropy.io.fits.hdu.hdulist.HDUList): #check type
+        fitsfile = filename
         opened=False
     else:
         raise Exception("cutout: Input file is wrong type (string or HDUList are acceptable).")
 
-    head = file[0].header.copy()
+    head = fitsfile[0].header.copy()
 
     if head['NAXIS'] > 2:
         raise DimensionError("Too many (%i) dimensions!" % head['NAXIS'])
-    cd1 = head.get('CDELT1') if head.get('CDELT1') else head.get('CD1_1')
-    cd2 = head.get('CDELT2') if head.get('CDELT2') else head.get('CD2_2')
-    if cd1 is None or cd2 is None:
-        raise Exception("Missing CD or CDELT keywords in header")
-    wcs = wcs.WCS(head)
 
-    xmin,xmax = numpy.max([0,xx-xw]),numpy.min([head['NAXIS1'],xx+xw])
-    ymin,ymax = numpy.max([0,yy-yw]),numpy.min([head['NAXIS2'],yy+yw])
+    xr = int(xw/2) #distance from center to edge to make below calculation easier
+    yr = int(yw/2)
+
+    xmin,xmax = numpy.max([0,xc-xr]),numpy.min([head['NAXIS1'],xc+xr])
+    ymin,ymax = numpy.max([0,yc-yr]),numpy.min([head['NAXIS2'],yc+yr])
 
     if xmax < 0 or ymax < 0:
         raise ValueError("Max Coordinate is outside of map: %f,%f." % (xmax,ymax))
     if ymin >= head.get('NAXIS2') or xmin >= head.get('NAXIS1'):
         raise ValueError("Min Coordinate is outside of map: %f,%f." % (xmin,ymin))
 
-    head['CRPIX1']-=xmin
+    head['CRPIX1']-=xmin 
     head['CRPIX2']-=ymin
     head['NAXIS1']=int(xmax-xmin)
     head['NAXIS2']=int(ymax-ymin)
@@ -56,14 +71,12 @@ def cutout(filename, xc, yc, xw=500, yw=500, outfile=None, clobber=True, verbose
     if head.get('NAXIS1') == 0 or head.get('NAXIS2') == 0:
         raise ValueError("Map has a 0 dimension: %i,%i." % (head.get('NAXIS1'),head.get('NAXIS2')))
 
-    img = file[0].data[ymin:ymax,xmin:xmax]
-    newfile = pyfits.PrimaryHDU(data=img,header=head)
+    img = fitsfile[0].data[ymin:ymax,xmin:xmax]
+    newfile = fits.PrimaryHDU(data=img,header=head)
     if verbose: print(("Cut image %s with dims %s to %s.  xrange: %f:%f, yrange: %f:%f" % (filename, file[0].data.shape,img.shape,xmin,xmax,ymin,ymax)))
 
-    if isinstance(outfile,str):
-        newfile.writeto(outfile,clobber=clobber)
+    newfile.writeto(outfile,clobber=clobber)
 
     if opened:
         file.close()
 
-    return newfile

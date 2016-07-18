2eea2d7fd43caadfef1cdba8a76ad69ad17ea0f2
Alex Hagen <mr.alex.hagen@gmail.com>
2016-07-18 12:09:27 -0400

    added eta to morphology and changed concentration to use eta

--- morphology.py
+++ morphology.py
@@ -1,7 +1,7 @@
 import numpy as np
 import scipy.interpolate as interp
 
-def concentration(radii, phot, interp_kind='linear', add_zero=True):
+def concentration(radii, phot, eta_radius=0.2, eta_radius_factor=1.5, interp_kind='linear', add_zero=True):
     """
     Calculates the concentration parameter
     C = 5 * log10(r_80 / r2_0) 
@@ -20,7 +20,11 @@ def concentration(radii, phot, interp_kind='linear', add_zero=True):
     if add_zero:
         radii = np.insert(radii, 0, 0)
         phot = np.insert(phot, 0, 0)
-    norm_phot = phot / np.max(phot)
+    eta_interp = interp.interp1d(eta(radii, phot), radii, kind=interp_kind)
+    eta_r = eta_radius_factor * eta_interp(eta_radius)
+    phot_interp = interp.interp1d(radii, phot, kind=interp_kind)
+    maxphot = phot_interp(eta_r)
+    norm_phot = phot / maxphot
     radius_interp = interp.interp1d(norm_phot, radii, kind=interp_kind)
     r20 = radius_interp(0.2)
     r80 = radius_interp(0.8)
@@ -28,3 +32,21 @@ def concentration(radii, phot, interp_kind='linear', add_zero=True):
     c = 5 * np.log10(r80 / r20)
     return c
     
+def eta(radii, phot):
+    """
+    eta = I(r) / \bar{I}(<r)
+    
+    radii -- 1d array of aperture photometry radii
+    phot -- 1d array of aperture photometry fluxes
+    
+    this is currently calculated quite naively, and probably could be done better
+    """
+    phot_area = np.pi * radii**2
+    phot_area_diff = np.ediff1d(phot_area, to_begin=phot_area[0])
+    I_bar = phot / (phot_area)
+    I_delta_r = np.ediff1d(phot, to_begin=phot[0]) / phot_area_diff
+    I_r = (I_delta_r[:-1] + I_delta_r[1:]) / 2 #lost last array element here
+    I_r = np.append(I_r, I_delta_r[-1]) #added it back in here
+    eta = I_r / I_bar
+    return eta
+
